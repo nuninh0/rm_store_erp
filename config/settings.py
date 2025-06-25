880aa39982d8483ddd3dba6c81ae72a492343c11
Bruno Gabriel <bruno.ggpls@gmail.com>
2025-06-24 22:15:45 -0300

    Corrige lógica de configuração da variável DEBUG

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
